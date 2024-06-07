1a480a1fc1e0f6cc1239bc6166ae7f5b95c5c1fa
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-06-07 11:47:14 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
+    console.log('Query', query);
 
-    // res.render('pages/search);
+    if(!query){
+        res.redirect('/');
+    return
+    }
 
+    let list = Pet.getFromName(query);
+    console.log('resultado de busca', list);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query,
+    });
 }
